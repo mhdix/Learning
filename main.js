114ea449fce3a6db0eea7a8ab15b904439b3fce2
mhdix <domanloom@gmail.com>
2024-10-01 04:43:10 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const  user = {name: 'mosi' , lName: 'mosa'}
+
+console.log(user.name)
+console.log('4' * '3' )
+
+
+const totalPrice = 220_000
+const dicountLimit = 200_000
+
+console.log(totalPrice > dicountLimit ? '10' : '5');
