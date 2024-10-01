58bc5c3a1e24cb0bab4e4f1c25c7d928b0703417
mhdix <domanloom@gmail.com>
2024-10-01 05:16:55 -0700

    logical operator

--- main.js
+++ main.js
@@ -8,3 +8,12 @@ const totalPrice = 220_000
 const dicountLimit = 200_000
 
 console.log(totalPrice > dicountLimit ? '10' : '5');
+
+//? Coupon Limit
+// logical operators 15
+
+const isActive = false
+const couponUsage = 10;
+const couponLimit = 20
+
+console.log(couponUsage < couponLimit && isActive ? 'you can usage coupon' : 'cant usage coupon' )
